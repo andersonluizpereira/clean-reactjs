3c8107bf388c09eec67832463b9c059b5631f9d7
Anderson Pereira <andersononus@hotmail.com>
2020-11-12 14:10:45 -0300

    feat: ensure login present error if email is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -52,6 +52,16 @@ describe('Login Component', () => {
     expect(passwordsStatus.textContent).toBe('🔴')
   })
 
+  test('Should show valid email state if Validation succeds', () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+    const emailStatus = sut.getByTestId('email-status')
+    expect(emailStatus.title).toBe('Tudo certo!')
+    expect(emailStatus.textContent).toBe('🟢')
+  })
+
   test('Should show valid password state if Validation succeds', () => {
     const { sut, validationStub } = makeSut()
     validationStub.errorMessage = null

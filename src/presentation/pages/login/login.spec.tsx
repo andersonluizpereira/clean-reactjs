10177d5f485d07891ba273157319f360ab265611
Anderson Pereira <andersononus@hotmail.com>
2020-12-14 11:57:45 -0300

    feat: prevent Login to submit form if its invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -115,4 +115,12 @@ describe('Login Component', () => {
     simulatedValidSubmit(sut)
     expect(authenticationSpy.callsCount).toBe(1)
   })
+
+  test('Should not call Authentication if form is invalid', () => {
+    const validationError = faker.random.words()
+    const { sut, authenticationSpy } = makeSut({ validationError })
+    populatedEmailField(sut)
+    fireEvent.submit(sut.getByTestId('form'))
+    expect(authenticationSpy.callsCount).toBe(0)
+  })
 })

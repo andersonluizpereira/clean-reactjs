a97afb54043dc332e49ce96a0bcce84404d84e3d
Anderson Pereira <andersononus@hotmail.com>
2021-01-05 19:14:58 -0300

    feat: ensure RequiredFieldValidation returns falsy if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,10 +1,16 @@
 import { RequiredFieldError } from '@/validation/errors'
 import { RequiredFieldValidation } from './required-field-validation'
+import faker from 'faker'
 
 describe('RequiredFieldValidation', () => {
   test('Should return if field is empty', () => {
     const sut = new RequiredFieldValidation('email')
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
+  })
+  test('Should return falsy if field is not empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate(faker.random.word())
+    expect(error).toBeFalsy()
   })
 })

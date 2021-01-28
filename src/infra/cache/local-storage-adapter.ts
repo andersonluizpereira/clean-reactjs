cceadca19104e94734818be4ba49baac40a00506
Anderson Pereira <andersononus@hotmail.com>
2021-01-28 19:24:58 -0300

    feat: ensure LocalStorageAdapter calls LocalStorageAdapter with correct values

---
+++ src/infra/cache/local-storage-adapter.ts
@@ -0,0 +1,6 @@
+import { SetStorage } from '@/data/protocols/cache/set-storage'
+export class LocalStorageAdapter implements SetStorage {
+  async set (key: string, value: any): Promise<void> {
+    localStorage.setItem(key, value)
+  }
+}

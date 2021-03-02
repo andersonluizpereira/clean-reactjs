7851545e7a77e25290be151012d91ede724a6dda
Anderson Pereira <andersononus@hotmail.com>
2021-03-02 19:43:15 -0300

    refactor: change validation to receive an object instead of value

--- src/main/factories/pages/signup/signup-validation-factory.spec.ts
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -1,13 +1,15 @@
-import { makeLoginValidation } from './login-validation-factory'
+import { makeSignUpValidation } from './signup-validation-factory'
 import { ValidationComposite } from '@/validation/validators'
 import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
 
 describe('LoginValidationFactory', () => {
   test('Should make ValidationComposite with correct validations', () => {
-    const composite = makeLoginValidation()
+    const composite = makeSignUpValidation()
     expect(composite).toEqual(ValidationComposite.build([
+      ...Builder.field('name').required().min(5).build(),
       ...Builder.field('email').required().email().build(),
-      ...Builder.field('password').required().min(5).build()
+      ...Builder.field('password').required().min(5).build(),
+      ...Builder.field('passwordConfirmation').required().min(5).build()
     ]))
   })
 })

0a2b178230653ace66ae4ef1f2e661bf3b61a3d1
Anderson Pereira <andersononus@hotmail.com>
2021-03-12 19:34:42 -0300

    feat: ensure AxiosHttpClient returns correct value on failure

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -64,5 +64,13 @@ describe('AxiosHttpClient', () => {
         body: axiosResponse.data
       })
     })
+    test('Should return correct error on axios.get', () => {
+      const { sut, mockedAxios } = makeSut()
+      mockedAxios.get.mockRejectedValueOnce({
+        response: mockHttpResponse()
+      })
+      const promise = sut.get(mockGetRequest())
+      expect(promise).toEqual(mockedAxios.get.mock.results[0].value)
+    })
   })
 })

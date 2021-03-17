6bd64649e43e104b63652a0a69f132dbbf54f8b5
Anderson Pereira <andersononus@hotmail.com>
2021-03-17 13:45:56 -0300

    refactor: move current account control to the api context

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -3,16 +3,23 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { SurveyList } from '@/presentation/pages'
 import { makeLogin } from '@/main/factories/pages/login/login-factory'
 import { makeSignUp } from '@/main/factories/pages/signup/signup-factory'
+import { ApiContext } from '@/presentation/contexts'
+import { setCurrentAccountAdapter } from '../adapters/current-account-adapter'
 
 const Router: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/login" exact component={makeLogin} />
-        <Route path="/signup" exact component={makeSignUp} />
-        <Route path="/" exact component={SurveyList} />
-      </Switch>
-    </BrowserRouter>
+    <ApiContext.Provider
+      value={{
+        setCurrentAccount: setCurrentAccountAdapter
+      }}>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" exact component={makeLogin} />
+          <Route path="/signup" exact component={makeSignUp} />
+          <Route path="/" exact component={SurveyList} />
+        </Switch>
+      </BrowserRouter>
+    </ApiContext.Provider>
   )
 }
 

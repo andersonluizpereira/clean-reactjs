3aba20734aed966d19e505fe97efdc64551c2d68
Anderson Pereira <andersononus@hotmail.com>
2021-03-28 17:49:05 -0300

    feat: add SurveyList composition

---
+++ src/main/factories/pages/survey-list/survey-list-factory.tsx
@@ -0,0 +1,11 @@
+import { SurveyList } from '@/presentation/pages'
+import React from 'react'
+import { makeRemoteLoadSurveyList } from '@/main/factories/usecases'
+
+export const makeSurveyList: React.FC = () => {
+  return (
+    <SurveyList
+      loadSurveyList={makeRemoteLoadSurveyList()}
+    />
+  )
+}
